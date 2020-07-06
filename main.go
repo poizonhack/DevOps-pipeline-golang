e63421b2685595d4e759d2fa96c33c3eaa990214
poizonhack <51056576+poizonhack@users.noreply.github.com>
2020-07-07 00:38:51 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,20 +1,25 @@
 package main
 
+
+
 import (
-   "log"
-   "net/http"
+
+	"log"
+
+	"net/http"
+
 )
 
-type Server struct{}
 
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-   w.WriteHeader(http.StatusOK)
-   w.Header().Set("Content-Type", "application/json")
-   w.Write([]byte(`{"message": ""Hello DevOps"}`))
-}
 
 func main() {
-   s := &Server{}
-   http.Handle("/", s)
-   log.Fatal(http.ListenAndServe(":8080", nil))
+
+	handler := http.HandlerFunc(PlayerServer)
+
+	if err := http.ListenAndServe(":8080", handler); err != nil {
+
+		log.Fatalf("could not listen on port 8080 %v", err)
+
+	}
+
 }
